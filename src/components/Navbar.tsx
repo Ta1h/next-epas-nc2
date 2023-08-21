594be0a0f5b5e46f1128afd3bb2981d205fd4f40
Talh <ralphtaoc071@gmail.com>
2023-08-21 12:29:02 +0700

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,77 @@
 "use client";
 
-import React from 'react'
+import { useState, useEffect, Fragment} from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
-const Navbar = () => {
-  return (
-    <div>
-        <Link href={"/"}>
-            <Image
-                priority
-                width="120"
-                height="120"
-                quality={100}
-                src="logo.svg"
-                alt="Logo"
-            />
+import { Button } from "@/components/ui/Button"
+import ScrollLink from './ui/ScrollLink';
 
-        </Link>
+const Navbar = () => {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        // if scroll down hide the navbar
+        setShow(false);
+      } else {
+        // if scroll up show the navbar
+        setShow(true);
+      }
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar);
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+  }, [controlNavbar, lastScrollY]);
+
+  return (
+    <div className='flex justify-between mx-10 my-5'>
+        <div className='px-8'>
+          <Link href={"/"}>
+              <Image
+                  priority
+                  width="100"
+                  height="140"
+                  quality={100}
+                  src="logo.svg"
+                  alt="Logo"
+              />
+          </Link>
+        </div>
+        <div className="flex-auto hidden z-50 lg:flex lg:justify-center lg:gap-x-12">
+          <Button variant="link">
+            <Link href="/">Home</Link>
+          </Button>
+          <Button variant="link">
+            <ScrollLink href="#programs">Programs</ScrollLink>
+          </Button>
+          <Button variant="link">
+            <ScrollLink href="#about">About</ScrollLink>
+          </Button>
+          <Button variant="link">
+            <ScrollLink href="#contacts">Contacts</ScrollLink>
+          </Button>
+        </div>
+        <div className=' hidden lg:flex lg:gap-x-5'>
+          <Button variant="outline" className='px-8 '>
+            <Link href="/">Login</Link>
+          </Button>
+          <Button variant="default" className='px-8'>
+            <Link href="/">Sign up</Link>
+          </Button>
+        </div>
     </div>
   )
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,15 +9,15 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "duration-100 rounded-lg bg-transparent hover:bg-black hover:text-white",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "duration-100 rounded-lg border-transparent border-2 hover:border-black hover:border-2",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "font-bold text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
