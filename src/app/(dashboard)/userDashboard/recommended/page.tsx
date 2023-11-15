1366f813d9b9bd8dc2677889c5e2888368e263a6
Talh <ralphtaoc071@gmail.com>
2023-11-15 22:15:55 +0800

    recommendation jsx

--- src/app/(dashboard)/userDashboard/recommended/page.tsx
+++ src/app/(dashboard)/userDashboard/recommended/page.tsx
@@ -42,34 +42,54 @@ const Page = () => {
   }, [])
 
   return (
-    <div className='p-10 h-full space-y-7'>
+    <div className="p-10 h-full space-y-7">
       {units.map((unit, index) => (
-        <div key={unit.id} className='flex-col rounded-md w-full shadow-[0px_3px_8px_0px_#00000024] min-h-80'>
-          <h1 className={`${colors[index]} text-2xl pl-5 py-3 text-white rounded-t-md`}>{unit.unitNumber}{unit.unitTitle}</h1>
-          <div>
-            {unit.lessons.map((lesson) => (
-              <div key={lesson.id} className='grid lg:grid-cols-4 lg:gap-x-26 lg:gap-y-5 p-7'>
-                {unit.score
-                  .filter(scores => scores.userEmail === userEmail && scores.lessonId === lesson.id)
-                  .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()) // Sort scores by date in descending order
-                  .slice(0, 1) // Take only the latest score
-                  .map((latestScore) => (
-                    <div key={latestScore.id}>
-                      {latestScore.lessonScore < 0.8 * latestScore.lessonLength && (
-                        <div>
-                          <div className='flex-col w-56 p-3 rounded-md shadow-[0px_3px_8px_0px_#00000024]'>
-                            <h2>{lesson.lessonNumber}</h2>
-                            <h2>{lesson.lessonTitle}</h2>
-                            <p>Score: {latestScore.lessonScore} / {latestScore.lessonLength}</p>
-                            <p>Time: {new Date(latestScore.date).toLocaleTimeString()}</p>
-                            <p>Date: {new Date(latestScore.date).toLocaleDateString()}</p>
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  ))}
-              </div>
-            ))}
+        <div key={unit.id} className="flex-col rounded-md w-full shadow-[0px_3px_8px_0px_#00000024] min-h-80">
+          <h1 className={`${colors[index]} text-2xl pl-5 py-3 text-white rounded-t-md`}>
+            {unit.unitNumber}
+            {unit.unitTitle}
+          </h1>
+          <div className="p-7 grid lg:grid-cols-3 lg:gap-5">
+            {unit.lessons
+              .filter((lesson) =>
+                unit.score.some(
+                  (score) =>
+                    score.userEmail === userEmail &&
+                    score.lessonId === lesson.id &&
+                    score.lessonScore < 0.6 * score.lessonLength
+                )
+              )
+              .map((lesson) => (
+                <div key={lesson.id} className="flex-col h-full p-3 rounded-md shadow-[0px_3px_8px_0px_#00000024]">
+                  {unit.score
+                    .filter(
+                      (scores) =>
+                        scores.userEmail === userEmail &&
+                        scores.lessonId === lesson.id 
+                    )
+                    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()) // Sort scores by date in descending order
+                    .slice(0, 1) // Take only the latest score
+                    .map((latestScore) => (
+                      <div key={latestScore.id}>
+                        {latestScore.lessonScore < 0.6 * latestScore.lessonLength && (
+                          <div>
+                            <h2 className='text-xl font-semibold'>{lesson.lessonNumber}</h2>
+                            <h2 className='h-16 flex items-center'>{lesson.lessonTitle}</h2>
+                            <div className='text-sm grid lg:grid-cols-3 justify-center border-2 rounded-md px-1'>
+                              <p>Score: </p>
+                              <p>Time: </p>
+                              <p>Date: </p>
+                              <p>{latestScore.lessonScore} / {latestScore.lessonLength}</p>
+                              <p>{new Date(latestScore.date).toLocaleTimeString()}</p>
+                              <p>{new Date(latestScore.date).toLocaleDateString()}</p>
+                            </div>
+                          </div> 
+                        )}
+                        
+                      </div>
+                    ))}
+                </div>
+              ))}
           </div>
         </div>
       ))}
